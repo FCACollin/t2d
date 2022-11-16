7d50ca4eb70c44837547e36da3760c2950a3a6b7
FCA Collin <fca.collin@gmail.com>
2022-11-16 16:30:41 +0100

    status 16 Nov 2022

--- R/export_xl.R
+++ R/export_xl.R
@@ -4,7 +4,11 @@
 #'
 #' @importFrom openxlsx createWorkbook addWorksheet writeData saveWorkbook
 #'
-export_xl <- function(..., info, dir, basenm) {
+export_xl <- function(...,
+                      info,
+                      dir,
+                      basenm,
+                      rdata = FALSE) {
 
   df_ls <- list(...)
   wb <- openxlsx::createWorkbook()
@@ -23,24 +27,32 @@ export_xl <- function(..., info, dir, basenm) {
 
   for (i in seq_along((df_ls))) {
     df <- df_ls[[i]]
-    ws_nm_info <- names(df_ls)[i]
+    ws_nm <- names(df_ls)[i]
     lbl_df <- lapply(df, attr, "label")
     lbl_df <- lapply(lbl_df, \(x) if (is.null(x)) "" else x)
     lbl_df <- as.data.frame(lbl_df, row.names = "label:")
-    openxlsx::addWorksheet(wb, ws_nm_info)
+    openxlsx::addWorksheet(wb, ws_nm)
     openxlsx::writeData(
       wb = wb,
-      sheet = ws_nm_info,
+      sheet = ws_nm,
       x = lbl_df,
       rowNames = TRUE,
       colNames = FALSE
     )
     openxlsx::writeData(
-      wb, ws_nm_info,
+      wb, ws_nm,
       x = df,
       rowNames = TRUE,
       startRow = 2
     )
+  }
+
+  if (rdata) {
+    filenm <- file.path(dir, paste0(basenm, ".RData"))
+    message("RData output (binary: ", filenm)
+    info_ls <- setNames(info_df, nm = ws_nm_info)
+    wcn <- c(info_ls, df_ls) # Weigh Correlation Network
+    save(wcn, file = filenm)
   }
 
   filenm <- file.path(dir, paste0(basenm, ".xlsx"))
